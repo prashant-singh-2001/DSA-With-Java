4ae47c0918a92466a61b7a89b9eb3aab19657bb8
Prashant Singh <ps47600@gmail.com>
2024-06-06 21:46:31 +0530

    Searchin complete

--- SearchingTechs.java
+++ SearchingTechs.java
@@ -26,36 +26,74 @@ class Search {
         }
         return new Result();
     }
-    
 
     public static Result binarySearch(int[] arr, int target) {
-        Arrays.sort(arr);
-        int m;
+
         int l = 0, r = arr.length - 1;
-        while (l < r) {
-            m = l + (r - l) / 2;
-            if (arr[m] == target)
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] == target) {
                 return new Result(target, m);
-            else if (arr[m] > target)
+            } else if (arr[m] > target) {
                 r = m - 1;
-            else
+            } else {
                 l = m + 1;
+            }
         }
+        return new Result();
+    }
+
+    public static Result interporlationSearch(int[] arr, int target) {
+
+        int l = 0, r = arr.length - 1;
+        while (l <= r && arr[l] <= target && arr[r] >= target) {
+            int pos = l + (((target - arr[l]) * (r - l)) / (arr[r] - arr[l]));
+            if (arr[pos] == target)
+                return new Result(target, pos);
+            else if (arr[pos] > target)
+                l = pos - 1;
+            else
+                r = pos + 1;
+        }
+
         return new Result();
     }
 }
 
 public class SearchingTechs {
     public static void main(String[] args) {
-        int[] arr = new int[] { 5, 47, 96, 5, 6, 6, 3, 5, 2, 5, 2, 25, 23, 4, 52, 5, 6, 3, 487, 9, 3, 4, 92, 3, 129,
-                825, 19, 846, 216, 549, 265, 195, 2, 65, 621, 549841, 198, 18654, 984, 31, 984, 6, 498, 46, 98, 46, 6,
-                2984, 26, 2195, 26, 519, 1, 5198, 198, 498, 9, 98, 4, 98, 4598298, 498, 161, 298 };
+        int[] arr = randomArray(100);
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
         Result rs = Search.linearSearch(arr, 52);
         if (rs.found) {
-            System.out.println("Data found at " + rs.index + 1 + " index");
+            System.out.println("Data found at " + rs.index  + " index");
         } else {
             System.out.println("Data is not in the array");
         }
+        rs = Search.binarySearch(arr, 52);
+        if (rs.found) {
+            System.out.println("Binary: Data found at " + rs.index  + " index");
+        } else {
+            System.out.println("Data is not in the array");
+        }
+        rs = Search.interporlationSearch(arr, 52);
+        if (rs.found) {
+            System.out.println("Interpolation : Data found at " + rs.index + " index");
+        } else {
+            System.out.println("Data is not in the array");
+        }
+    }
 
+    private static int[] randomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = randomInt(1, 200);
+        }
+        return arr;
+    }
+
+    private static int randomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
     }
 }
