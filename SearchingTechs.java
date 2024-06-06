09dec6e617a39f35195dc80bb1b6d66d67f4ebc1
Prashant Singh <ps47600@gmail.com>
2024-06-06 14:37:21 +0530

    Looped Binary Search

--- SearchingTechs.java
+++ SearchingTechs.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Result {
     int data;
     int index;
@@ -24,6 +26,20 @@ class Search {
         }
         return new Result();
     }
+
+    public static Result binarySearch(int[] arr, int target) {
+        Arrays.sort(arr);
+        int m;
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (arr[m] == target) 
+            return new Result(target, m);
+            else if(arr[m]>target) r=m-1;
+            else l=m+1;
+        }
+        return new Result();
+    }
 }
 
 public class SearchingTechs {
@@ -31,13 +47,12 @@ public class SearchingTechs {
         int[] arr = new int[] { 5, 47, 96, 5, 6, 6, 3, 5, 2, 5, 2, 25, 23, 4, 52, 5, 6, 3, 487, 9, 3, 4, 92, 3, 129,
                 825, 19, 846, 216, 549, 265, 195, 2, 65, 621, 549841, 198, 18654, 984, 31, 984, 6, 498, 46, 98, 46, 6,
                 2984, 26, 2195, 26, 519, 1, 5198, 198, 498, 9, 98, 4, 98, 4598298, 498, 161, 298 };
-            Result rs=Search.linearSearch(arr, 52);
-            if(rs.found){
-                System.out.println("Data found at "+rs.index+1+" index");
-            }
-            else{
-                System.out.println("Data is not in the array");
-            }
-        
+        Result rs = Search.linearSearch(arr, 52);
+        if (rs.found) {
+            System.out.println("Data found at " + rs.index + 1 + " index");
+        } else {
+            System.out.println("Data is not in the array");
+        }
+
     }
 }
