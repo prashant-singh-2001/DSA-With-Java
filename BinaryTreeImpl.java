edf08159d7f1f7c432e4ce09e414b86d023c6903
Prashant Singh <ps47600@gmail.com>
2024-06-07 13:52:36 +0530

    Base is ready

--- BinaryTreeImpl.java
+++ BinaryTreeImpl.java
@@ -1,8 +1,10 @@
 final class TreeNode{
+    private static final int NaN = 0;
     public int data;
     public TreeNode left;
     public TreeNode right;
     TreeNode(){
+        data=NaN;
         left=null;
         right=null;
     }
@@ -19,10 +21,33 @@ final class TreeNode{
 class BinaryTree{
     private TreeNode root;
     BinaryTree(){
-        root=new TreeNode();
+        // root=new TreeNode();
+        root=null;
     }
     BinaryTree(TreeNode root){
         this.root=root;
+    }
+    public void insert(int data){
+        if(root==null){
+            root=new TreeNode(data);
+        }else{
+            insertRecur(root,data);
+        }
+    }
+    public void insertRecur(TreeNode root,int data){
+        if(root.data>data){
+            if(root.left==null){
+                root.left=new TreeNode(data);
+            }else{
+                insertRecur(root.left, data);
+            }
+        }else{
+            if(root.right==null){
+                root.right=new TreeNode(data);
+            }else{
+                insertRecur(root.right, data);
+            }
+        }
     }
 }
 public class BinaryTreeImpl {
