e44bc6ed156b96a0e4ab11b6b71878c9657402eb
Prashant Singh <ps47600@gmail.com>
2024-06-19 11:45:50 +0530

    Addded insertion and deletion

--- BTreeImp.java
+++ BTreeImp.java
@@ -3,17 +3,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
 class BTree {
     private class InnerBTree {
         private static final int NaN = 0;
         private int data;
-        private int left;
+        private BTree leftSubTree;
         private BTree rightSubTree;
 
         InnerBTree() {
             data = NaN;
-            left = NaN;
+            leftSubTree = null;
             rightSubTree = null;
         }
 
@@ -22,9 +21,9 @@ class BTree {
             this.data = data;
         }
 
-        InnerBTree(int data, int left, BTree rightSubTree) {
+        InnerBTree(int data, BTree leftSubTree, BTree rightSubTree) {
             this.data = data;
-            this.left = left;
+            this.leftSubTree = leftSubTree;
             this.rightSubTree = rightSubTree;
         }
     }
@@ -40,9 +39,83 @@ class BTree {
     }
 
     BTree(BTree b) {
-        root = new InnerBTree(b.root.data, b.root.left, b.root.rightSubTree);
+        root = new InnerBTree(b.root.data, b.root.leftSubTree, b.root.rightSubTree);
+    }
+
+    public boolean insert(int data) {
+        if (root.data == InnerBTree.NaN) {
+            root.data = data;
+            return true;
+        }
+        return insertRec(root, data);
+    }
+
+    private boolean insertRec(InnerBTree node, int data) {
+        if (data < node.data) {
+            if (node.leftSubTree == null) {
+                node.leftSubTree = new BTree(data);
+                return true;
+            } else {
+                return node.leftSubTree.insert(data);
+            }
+        } else if (data > node.data) {
+            if (node.rightSubTree == null) {
+                node.rightSubTree = new BTree(data);
+                return true;
+            } else {
+                return node.rightSubTree.insert(data);
+            }
+        } else {
+            return false; // Duplicate data
+        }
+    }
+
+    public int delete(int data) {
+        if (root == null || root.data == InnerBTree.NaN) {
+            return InnerBTree.NaN;
+        }
+        root = deleteRec(root, data);
+        return data;
+    }
+
+    private InnerBTree deleteRec(InnerBTree node, int data) {
+        if (node == null || node.data == InnerBTree.NaN) {
+            return null;
+        }
+
+        if (data < node.data) {
+            if (node.leftSubTree != null) {
+                node.leftSubTree.root = deleteRec(node.leftSubTree.root, data);
+            }
+        } else if (data > node.data) {
+            if (node.rightSubTree != null) {
+                node.rightSubTree.root = deleteRec(node.rightSubTree.root, data);
+            }
+        } else {
+            if (node.leftSubTree == null) {
+                return (node.rightSubTree != null) ? node.rightSubTree.root : null;
+            } else if (node.rightSubTree == null) {
+                return (node.leftSubTree != null) ? node.leftSubTree.root : null;
+            }
+
+            node.data = minValue(node.rightSubTree.root);
+            node.rightSubTree.root = deleteRec(node.rightSubTree.root, node.data);
+        }
+
+        return node;
+    }
+
+    private int minValue(InnerBTree node) {
+        int minValue = node.data;
+        while (node.leftSubTree != null && node.leftSubTree.root != null && node.leftSubTree.root.data != InnerBTree.NaN) {
+            minValue = node.leftSubTree.root.data;
+            node = node.leftSubTree.root;
+        }
+        return minValue;
     }
 }
+
+
 
 public class BTreeImp {
 
